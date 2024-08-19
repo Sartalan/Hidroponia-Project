f83c6a7dc3e41bfb99b19b36979a6f3c65aebab2
sarti <elpinguinoalan@gmail.com>
2024-08-18 22:09:00 -0600

    Code Improvement | Temperature Sensor

--- Code/Hidroponia - Code/src/main.cpp
+++ Code/Hidroponia - Code/src/main.cpp
@@ -1,34 +1,116 @@
 
+
 #include <Arduino.h>
-#include <TimerOne.h>            // Incluir Librería TimerOne
+#include <TimerOne.h> // Incluir Librería TimerOne
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define Disparo_pin 7
 
-int ge=0,pot;
-int disparo = 7;
+const int pinDatosDQ = 3;
+OneWire oneWireObjeto(pinDatosDQ);
+DallasTemperature TemperatureSensor(&oneWireObjeto);
 
-void Zero(){
-  ge = 0;
+int GradoElectrico = 0;
+int Value;
+int Test;
+
+void TemperatureTesting(){
+  Test = analogRead(A1);
+  Test = map(Test, 0, 1023, 15, 30);
+  Serial.print("------");
+  Serial.print("Temp: ");
+  Serial.print(Test);
+  Serial.println(" C");
 }
 
-void Grado_Electrico(){
-  ge++;
-  if(pot==ge){
-    digitalWrite(disparo,HIGH);
-    digitalWrite(disparo,LOW);
+void Zero()
+{ // g = 0
+  GradoElectrico = 0;
+}
+
+void Disparo()
+{
+  GradoElectrico++;
+  if (Value == GradoElectrico)
+  {
+    digitalWrite(Disparo_pin, HIGH);
+    digitalWrite(Disparo_pin, LOW);
   }
 }
 
 void setup()
 {
   Serial.begin(9600);
-  attachInterrupt(0,Zero,CHANGE);
+  TemperatureSensor.begin();
+  attachInterrupt(0, Zero, CHANGE);
   Timer1.initialize(46);
-  Timer1.attachInterrupt(Grado_Electrico);
-  pinMode(disparo,OUTPUT);
-  digitalWrite(disparo,LOW);
+  Timer1.attachInterrupt(Disparo);
+  pinMode(Disparo_pin, OUTPUT);
+  digitalWrite(Disparo_pin, LOW);
 }
 
 void loop()
 {
-  pot = analogRead(0);
-  pot = map(pot,0,1023,0,179);
+  Value = analogRead(0);
+  Value = map(Value, 0, 1023, 0, 179);
+  // Serial.println(Value);
+
+  TemperatureTesting();
+
+
+  //TemperatureSensor.requestTemperatures();
+  //Serial.print("Temperatura: ");
+  //Serial.print(TemperatureSensor.getTempCByIndex(0));
+  //Serial.println(" C");
 }
+
+/* Sensor Testing
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Arduino.h>
+const int pinDatosDQ = 3;
+OneWire oneWireObjeto(pinDatosDQ);
+DallasTemperature sensorDS18B20(&oneWireObjeto);
+
+void printAddress(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    Serial.print("0x");
+    if (deviceAddress[i] < 0x10) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+    if (i < 7) Serial.print(", ");
+  }
+  Serial.println("");
+}
+
+void setup() {
+  Serial.begin(9600);
+  sensorDS18B20.begin();
+
+  // Buscamos los sensores conectados
+  Serial.println("Buscando sensores");
+  Serial.println("Encontrados: ");
+  int sensoresConectados = sensorDS18B20.getDeviceCount();
+  Serial.print(sensoresConectados);
+  Serial.println(" sensores");
+
+  // Si hemos encontrado uno mostramos su dirección
+  if (sensoresConectados >= 1) {
+    DeviceAddress termometro;
+
+    Serial.println("Imprimiendo direcciones...");
+    for (int i = 0;  i < sensoresConectados;  i++)
+    {
+      Serial.print("Sensor ");
+      Serial.print(i + 1);
+      Serial.print(" : ");
+      sensorDS18B20.getAddress(termometro, i);
+      printAddress(termometro);
+    }
+  }
+}
+
+void loop() {
+}
+*/
