d913c49714946805e0e3dd2657fea4f3536f4dda
Sartalan <alanfacundobarrera@gmail.com>
2024-09-12 14:08:15 -0300

    Code Improvement | Sensors Added

--- Code/Embedded Code/src/main.cpp
+++ Code/Embedded Code/src/main.cpp
@@ -1,12 +1,41 @@
 #include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "DHT.h"
 
-void setup(){
-Serial.begin(115200);
-while(!Serial) {}
+// Data wire is conntec to the Arduino digital pin 4
+#define ONE_WIRE_BUS 7
+#define DHTPIN 4 // what pin we're connected to
+#define DHTTYPE DHT11
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(ONE_WIRE_BUS);
+DHT dht(DHTPIN, DHTTYPE);
+
+// Pass our oneWire reference to Dallas Temperature sensor
+DallasTemperature S_Temperature(&oneWire);
+
+void setup()
+{
+  Serial.begin(9600);
+  S_Temperature.begin();
+  dht.begin();
 }
 
-void loop(){
-  Serial.println("Mensaje a Raspberry Pi");
-  Serial.write("Lol");
-  delay(1000);  
+void loop()
+{
+  S_Temperature.requestTemperatures();
+  Serial.print("Celsius temperature: ");
+  Serial.print(S_Temperature.getTempCByIndex(0));
+
+  float h = dht.readHumidity();
+  delay(1000);
+
+  if (isnan(h))
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
 }
