b094e325157091462a4f2322c71aace6ec042474
Sartalan <elpinguinoalan@gmail.com>
2024-10-04 00:32:01 -0300

    Code Improvement [Python Server] | IP Lectures to send to the Sense Hat

--- Code/Embedded Code/src/main.cpp
+++ Code/Embedded Code/src/main.cpp
@@ -7,17 +7,20 @@
 #define ONE_WIRE_BUS 7  //? DS18B20
 #define DHTPIN 4        //? DHT11
 #define DHTTYPE DHT11   
-#define Disparo_Bomba 3
+#define Disparo_Bomba 2
 #define Disparo_Lampara 5
 #define Caudal_Pin 6
 
 int GradoElectrico = 0;
 int LDR_Value, Lluvia_Value;
+int Tiempo = 0;
+
 float Caudal_Value;
 unsigned long Caudal_Pulsos = 0;
 unsigned long Previous_Time = 0;
 
 unsigned long Previus_Time_Bomba = 0;
+unsigned long Previus_Time_Bomba_Dos = 0;
 bool Turn_Pump = false;
 // Setup a oneWire instance to communicate with any OneWire devices
 OneWire oneWire(ONE_WIRE_BUS);
@@ -28,26 +31,44 @@ DallasTemperature S_Temperature(&oneWire);
 void GradoZero()
 {
   GradoElectrico = 0;
+  Tiempo++;
+  Serial.println(Tiempo);
+
+  if(Tiempo == 9000){
+    Serial.println("LLEGUÉ A 9000");
+    Tiempo = 0;
+    Turn_Pump = !Turn_Pump;
+  }
+
+   
+  if (Turn_Pump == 1) {
+    digitalWrite(Disparo_Bomba, HIGH);
+    Serial.println("Me prendí");
+  }
+
+  else {
+    digitalWrite(Disparo_Bomba, LOW);
+    Serial.println("Me Apagué");
+  }
+
+    Serial.print("Valor bool ");
+    Serial.println(Turn_Pump);
+  
 }
 
 void Disparo()
 {
-   GradoElectrico++;
+   //Serial.println(GradoElectrico);
 
 //? Lógica para manejar el disparo de la lampara.
 
-  if (LDR_Value == GradoElectrico)
-  {
-    Serial.print(GradoElectrico);
-    digitalWrite(Disparo_Lampara, HIGH);
-    digitalWrite(Disparo_Lampara, LOW);
-  }
-//Code
+ 
+  
 
+   GradoElectrico++;
+//Code
 //? Lógica para manejar el disparo de la bomba de agua | (Intervalo de 5s)
-  if (Turn_Pump == true){
-    digitalWrite(Disparo_Bomba, LOW);
-  }
+
 }
 
 void setup()
@@ -60,7 +81,7 @@ void setup()
   pinMode(Disparo_Bomba, OUTPUT);
   pinMode(Disparo_Lampara, OUTPUT);
   // Timer
-  attachInterrupt(0, GradoZero, CHANGE);   //"0" es el pin 2
+  attachInterrupt(1, GradoZero, CHANGE);   //"0" es el pin 2
   Timer1.initialize(55);                 // Seteado a 55us
   Timer1.attachInterrupt(Disparo);
 
@@ -81,38 +102,23 @@ void loop()
   if (millis() - Previous_Time >= 1000) //? Cada un segundo devuelve el valor del YF-S201
   {
     Caudal_Value = (float)Caudal_Pulsos / 450.0; // Aproximadamente 450 pulsos por litro
-    Serial.print("Caudal (L/min): ");
-    Serial.println(Caudal_Value);
+   // Serial.print("Caudal (L/min): ");
+    //Serial.println(Caudal_Value);
     Caudal_Pulsos = 0; // Reiniciar contador
     Previous_Time = millis();
   }
-  if (millis() - Previus_Time_Bomba >= 5000){
-    Turn_Pump = !Turn_Pump;
-    Previus_Time_Bomba = millis();
-  }
 
-  Serial.print("La bomba está: ");
-  Serial.println(Turn_Pump);
-  //* Timer para 
   //* Toma la lectura de todos los sensores del circuito
   LDR_Value = analogRead(A0);
   LDR_Value = map(LDR_Value, 0, 1023, 0, 178);
   Lluvia_Value = analogRead(A1);
   Lluvia_Value = map(Lluvia_Value, 0, 1023, 0, 10);
   S_Temperature.requestTemperatures();
-  float Temperature = (S_Temperature.getTempCByIndex(0));
-  float Humidity = S_Humidity.readHumidity();
+  //float Temperature = (S_Temperature.getTempCByIndex(0));
+  //float Humidity = S_Humidity.readHumidity();
 
   //* Envía los datos a la Raspberry Pi 
-  Serial.print("Lluvia: ");
-  Serial.println(Lluvia_Value);
-  Serial.print("LDR: ");
-  Serial.println(LDR_Value);
-  Serial.print("Temperatura: ");
-  Serial.println(Temperature);
-  Serial.print("Humedad: ");
-  Serial.println(Humidity);
-  delay(1000);
+
 
   //* Testea sí los sensores funcionan o no.
 }
--- Code/Raspberry Pi Code/app.py
+++ Code/Raspberry Pi Code/app.py
@@ -1,13 +1,39 @@
+from flask import Flask # type: ignore
 import subprocess 
 import time
 import json
 import socket
+import psutil
 
-from flask import Flask # type: ignore
+##? Obtiene la IP Local
+Host = socket.gethostname()
+Local_IP = socket.gethostbyname(Host)
 
+List_IP = []
+
+##? Obtiene todas las interfaces de red y sus direcciones IP
+IP = psutil.net_if_addrs()
+
+for interface, addr_info in IP.items():
+    for addr in addr_info:
+        ##? Filtrar solo las direcciones IPv4 que no sean la local | (Local_IP)
+        if addr.family == socket.AF_INET and addr.address != Local_IP:
+            ## print(f"Interfaz: {interface} - IP: {addr.address}")
+            List_IP.append(addr.address)
+
+print(List_IP) 
+##? Printea todas; almacena la de valor [0]
+IP_DATA = List_IP[0]
+
+##? ---
+##? Obtiene las direcciones de IP públicas brindadas
+##? Por el router y las compara con la IP Local; si ésta
+##? Es diferente, la almacena en una lista. La IP[0] de la 
+##? Lista será la mostrada en pantalla.
+##todo: Este modo de resolver el problema podría generar un bug sí 
+##todo: Hay más de una interfáz wifi en el servidor; se debe tener en cuenta.
+##* ----------------------------------
 ##* Este es el archivo main a ejecutar
-
-
 
 subprocess.Popen(["python3", "./src/filter.py"],
                  stderr=subprocess.DEVNULL,
